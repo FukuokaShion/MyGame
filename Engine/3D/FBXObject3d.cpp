4589cac7af11a9ab297377eba9c654caba4cc90f
Fukuoka_Shion <k021g1250@g.neec.ac.jp>
2023-06-22 13:04:48 +0900

    エラー警告削除

--- Engine/3D/FBXObject3d.cpp
+++ Engine/3D/FBXObject3d.cpp
@@ -280,8 +280,7 @@ void FBXObject3d::Draw(ID3D12GraphicsCommandList* cmdList)
 }
 
 void FBXObject3d::PlayAnimation(float speed, bool isLoop){
-	//animationTime = frameTime * speed;
-	animationTime = frameTime;
+	animationTime = static_cast<FbxLongLong>(frameTime.Get() * speed);
 	FbxScene* fbxScene = fbxmodel->GetFbxScene();
 	//0番のアニメーションを取得
 	FbxAnimStack* animstack = fbxScene->GetSrcObject<FbxAnimStack>(0);
