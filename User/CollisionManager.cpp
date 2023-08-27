53da01173d91399dc476cb2a9dc38e0b9b6353fa
Fukuoka_Shion <k021g1250@g.neec.ac.jp>
2023-08-28 08:58:33 +0900

    リファクタリング カプセル化

--- User/CollisionManager.cpp
+++ User/CollisionManager.cpp
@@ -13,7 +13,8 @@ bool CollisionManager::isEnemyHit;
 void CollisionManager::CheckCollision() {
 	//“G‚ÌUŒ‚
 	Cylinder playerBody = player_->GetBodyHitBox();
-	if (Collision::CheckSphere2Cylinder(enemy_->attackHitBox, playerBody)) {
+	Sphere enemyAttack = enemy_->GetAttackHitBox();
+	if (Collision::CheckSphere2Cylinder(enemyAttack, playerBody)) {
 		//“G‚ÌUŒ‚
 		if (enemy_->GetIsAttack()) {
 			if (isPlayerHit == false) {
@@ -28,7 +29,8 @@ void CollisionManager::CheckCollision() {
 	//ƒvƒŒƒCƒ„[‚ÌUŒ‚
 	Vector3 hitPos;
 	Sphere playerAttack = player_->GetAttackHitBox();
-	if (Collision::CheckSphere2Cylinder(playerAttack, enemy_->bodyHitBox, &hitPos)) {
+	Cylinder enemyBody = enemy_->GetBodyHitBox();
+	if (Collision::CheckSphere2Cylinder(playerAttack, enemyBody, &hitPos)) {
 		//ƒvƒŒƒCƒ„[‚ÌUŒ‚
 		if (player_->GetIsAttack()) {
 			if (isEnemyHit == false) {
@@ -69,14 +71,16 @@ void CollisionManager::CheakCol() {
 Vector3 CollisionManager::Body2Body() {
 	Vector3 velocity = { 0,0,0 };
 	Cylinder playerBody = player_->GetBodyHitBox();
-	if (Collision::CheckCylinder2Cylinder(enemy_->bodyHitBox, playerBody)) {
+	Cylinder enemyBody = enemy_->GetBodyHitBox();
+
+	if (Collision::CheckCylinder2Cylinder(enemyBody, playerBody)) {
 		//‰Ÿ‚µo‚µˆ—
 		//ˆê•û“I‚ÉƒvƒŒƒCƒ„[‚ª‰Ÿ‚³‚ê‚é
-		Vector3 distance = playerBody.center - enemy_->bodyHitBox.center;
+		Vector3 distance = playerBody.center - enemyBody.center;
 		velocity = distance;
 
 		velocity.nomalize();
-		velocity *= playerBody.radius + enemy_->bodyHitBox.radius;
+		velocity *= playerBody.radius + enemyBody.radius;
 
 		velocity -= distance;
 	}
--- User/ENEMY/Enemy.h
+++ User/ENEMY/Enemy.h
@@ -12,51 +12,72 @@ class Player;
 
 class Enemy {
 public:
+	//設定
 	Enemy();
 	~Enemy();
-
 	void Initialize();
-	void SetPlayerTransform(Transform* playerWtf) { this->playerWtf = playerWtf; };
 
+	//更新
 	void Update();
+
+	//描画
 	void Draw();
 	void ObjDraw();
 
+	//状態以降
+	void TransitionTo(EnemyState* state);
+
+	//座標
+	
+	//プレイヤー座標
+	void SetPlayerTransform(Transform* playerWtf) { this->playerWtf = playerWtf; };
+
+	//攻撃当たり判定
+	Sphere GetAttackHitBox() { return attackHitBox; };
+	void SetAttackRad(float newRad) { attackHitBox.radius = newRad; };
+	void SetAttackPos(Vector3 newPos) { attackHitBox.center = newPos; };
+
+	//体当たり判定
+	Cylinder GetBodyHitBox() { return bodyHitBox; };
+
+	//被ダメ処理
+	void OnCollision(int damage, Vector3 hitPos);
+
+	//ステータス
+	bool IsLive() { return hp->IsLive(); };
+	int GetHp() { return hp->GetHp(); };
 	bool GetIsAttack() { return isAttack; };
 	int GetPower() { return power; };
 	void SetIsAttack(bool isAttack) { this->isAttack = isAttack; };
 	void setPower(int power) { this->power = power; };
 
-	void OnCollision(int damage,Vector3 hitPos);
-
-	void TransitionTo(EnemyState* state);
-
-	bool IsLive() { return hp->IsLive(); };
-	int GetHp() { return hp->GetHp(); };
-
+	//弾生成
 	void CreatBullet(Vector3 pos, Vector3 velocity, int liveLimit, int stayTime = 0);
 
 public:
-	//体当たり判定
-	Cylinder bodyHitBox;
-	//攻撃当たり判定
-	Sphere attackHitBox;
 	//プレイヤー座標
 	Transform* playerWtf = nullptr;
+	
 	//オブジェクト
 	FBXObject3d* fbxObject3d_ = nullptr;
-
 
 	//球
 	std::list<std::unique_ptr<EnemyBullet>> bullets;
 private:
-	//モデル
-	FBXModel* fbxModel_ = nullptr;
-
 	//行動
 	EnemyState* state_ = nullptr;
-	//ステータス
+	
+	//体力
 	EnemyHp* hp = nullptr;
+
+	//体当たり判定
+	Cylinder bodyHitBox;
+
+	//攻撃当たり判定
+	Sphere attackHitBox;
+
+	//モデル
+	FBXModel* fbxModel_ = nullptr;
 
 	//攻撃判定
 	bool isAttack;
@@ -64,6 +85,6 @@ private:
 
 	EnemyParticle* particle = nullptr;
 
-	//球
-	//std::list<std::unique_ptr<EnemyBullet>> bullets;
+	//弾
+
 };
