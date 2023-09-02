610ca885b234928ad8659e44d2fa84bfb347a0cc
Fukuoka_Shion <k021g1250@g.neec.ac.jp>
2023-09-02 22:50:20 +0900

    フィールドモデル変更

--- User/FIELD/Field.cpp
+++ User/FIELD/Field.cpp
@@ -6,31 +6,93 @@ Field::Field() {
 
 void Field::Initialize() {
 	//”wŒi
-	floorMD = Model::LoadFromOBJ("floor");
-	floor = Object3d::Create();
-	floor->SetModel(floorMD);
-	floor->wtf.position = (Vector3{ 0, -10, 0 });
-
 	skydomeMD = Model::LoadFromOBJ("skydome");
 	skydome = Object3d::Create();
 	skydome->SetModel(skydomeMD);
 	skydome->wtf.scale = (Vector3{ 1000, 1000, 1000 });
 
+	floorMD = Model::LoadFromOBJ("floor");
+	floor = Object3d::Create();
+	floor->SetModel(floorMD);
+
+	waterMD = Model::LoadFromOBJ("water");
+	water = Object3d::Create();
+	water->SetModel(waterMD);
+	water->wtf.scale = { 2,1,2 };
+
+	rock01MD = Model::LoadFromOBJ("rock01");
+	rock02MD = Model::LoadFromOBJ("rock02");
+
+	for (int i = 0; i < 3; i++) {
+		rock01[i] = Object3d::Create();
+		rock01[i]->SetModel(rock01MD);
+		rock02[i] = Object3d::Create();
+		rock02[i]->SetModel(rock02MD);
+	}
+	
+	float PI = 3.141592f;
+
+	rock01[0]->wtf.position = { 42,0,65 };
+	rock01[0]->wtf.scale = { 5,5,5 };
+	rock01[0]->wtf.rotation = { 0,0,0 };
+	
+	rock01[1]->wtf.position = { 122,0,10 };
+	rock01[1]->wtf.scale = { 5,5,5 };
+	rock01[1]->wtf.rotation = { PI/4,0,0 };
+
+	rock01[2]->wtf.position = { -26,0,-73 };
+	rock01[2]->wtf.scale = { 5,5,5 };
+	rock01[2]->wtf.rotation = { 0,0,0 };
+
+	rock02[0]->wtf.position = { -45,0,105 };
+	rock02[0]->wtf.scale = { 12,12,12 };
+	rock02[0]->wtf.rotation = { 0,0,0 };
+
+	rock02[1]->wtf.position = { 75,0,-75 };
+	rock02[1]->wtf.scale = { 10,10,10 };
+	rock02[1]->wtf.rotation = { 0,-3.0f * PI / 4.0f,0 };
+
+	rock02[2]->wtf.position = { -91,0,-49 };
+	rock02[2]->wtf.scale = { 8,8,8 };
+	rock02[2]->wtf.rotation = { 0,3.0f * PI / 20.0f,0 };
 }
 
 Field::~Field() {
-	delete floor;
-	delete floorMD;
 	delete skydome;
 	delete skydomeMD;
+	
+	delete floor;
+	delete floorMD;
+	delete water;
+	delete waterMD;
+	
+	for (int i = 0; i < 3; i++) {
+		delete rock01[i];
+		delete rock02[i];
+	}
+
+	delete rock01MD;
+	delete rock02MD;
 }
 
 void Field::Update() {
-	floor->Update();
 	skydome->Update();
+	floor->Update();
+	water->Update();
+	for (int i = 0; i < 3; i++) {
+		rock01[i]->Update();
+		rock02[i]->Update();
+	}
 }
 
 void Field::Draw() {
-	floor->Draw();
 	skydome->Draw();
+
+	floor->Draw();
+	water->Draw();
+	
+	for (int i = 0; i < 3; i++) {
+		rock01[i]->Draw();
+		rock02[i]->Draw();
+	}
 }
--- User/FIELD/Field.h
+++ User/FIELD/Field.h
@@ -13,9 +13,17 @@ public:
 	void Draw();
 private:
 	//îwåiÇ‚è∞
-	Object3d* floor = nullptr;
-	Model* floorMD = nullptr;
 	Object3d* skydome = nullptr;
 	Model* skydomeMD = nullptr;
+
+	Object3d* floor = nullptr;
+	Model* floorMD = nullptr;
+	Object3d* water = nullptr;
+	Model* waterMD = nullptr;
+
+	Object3d* rock01[3];
+	Model* rock01MD = nullptr;
+	Object3d* rock02[3];
+	Model* rock02MD = nullptr;
 
 };
