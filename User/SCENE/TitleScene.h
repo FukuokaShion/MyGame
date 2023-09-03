9ab64f34c90cc7cdc0458e410252480860e5c54a
Fukuoka_Shion <k021g1250@g.neec.ac.jp>
2023-09-03 15:37:19 +0900

    タイトル画面変更

--- User/SCENE/TitleScene.h
+++ User/SCENE/TitleScene.h
@@ -1,6 +1,8 @@
 #pragma once
 #include"SceneState.h"
 #include"Sprite.h"
+#include "Object3d.h"
+#include"Audio.h"
 
 class TitleScene : public SceneState {
 public:
@@ -20,7 +22,27 @@ private:
 	void StateTransition() override;
 	
 private:
+	//サウンド
+	Audio* audio = nullptr;
+	IXAudio2SourceVoice* pSourceVoice = nullptr;
+
+	//カメラ
+	Camera* camera = nullptr;
+
 	//画像
 	SpriteCommon* spriteCommon = nullptr;
 	Sprite* basePic;
+
+	//obj
+	Object3d* skydome = nullptr;
+	Model* skydomeMD = nullptr;
+	Object3d* water = nullptr;
+	Model* waterMD = nullptr;
+	Object3d* coast = nullptr;
+	Model* coastMD = nullptr;
+
+	Object3d* rock01[2];
+	Model* rock01MD = nullptr;
+	Object3d* rock02[2];
+	Model* rock02MD = nullptr;
 };
